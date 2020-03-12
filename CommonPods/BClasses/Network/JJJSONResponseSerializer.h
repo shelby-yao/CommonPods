582325dd131ff86d025f65715cf012a7c66f748b
姚晓俊 <shelby_yao@163.com>
2020-03-12 15:10:29 +0800

    fix

--- CommonPods/BClasses/Network/JJJSONResponseSerializer.h
+++ CommonPods/BClasses/Network/JJJSONResponseSerializer.h
@@ -5,9 +5,8 @@
 //  Created by Shelby on 2019/11/30.
 //
 
-
 #import <Foundation/Foundation.h>
-#import "JKNetworking.h"
+#import "AFNetworking.h"
 //NS_ASSUME_NONNULL_BEGIN
 static NSString *const JsonResponseSerializerKey = @"body";
 static NSString *const JsonResponseSerializerBodyKey = @"statusCode";
